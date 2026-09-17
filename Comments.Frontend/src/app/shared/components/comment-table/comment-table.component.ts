b756ccfbeed257bd55b0fe3c3ea2ada9c88f3008
khajiitvaper2017 <kanalgera@gmail.com>
2026-09-17 17:41:41 +0300

    table view doesn't need search plumbing

--- Comments.Frontend/src/app/shared/components/comment-table/comment-table.component.ts
+++ Comments.Frontend/src/app/shared/components/comment-table/comment-table.component.ts
@@ -2,20 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommentItem } from '@app/core/models/comment.models';
 import { HomePageLabelPipe } from '@app/shared/pipes/home-page-label.pipe';
-import { HighlightSearchPipe } from '@app/shared/pipes/highlight-search.pipe';
 
 @Component({
   selector: 'app-comment-table',
   standalone: true,
-  imports: [CommonModule, HomePageLabelPipe, HighlightSearchPipe],
+  imports: [CommonModule, HomePageLabelPipe],
   templateUrl: './comment-table.component.html',
 })
 export class CommentTableComponent {
   @Input() comments: CommentItem[] = [];
   @Input() sort = 'createdAt';
   @Input() descending = true;
-  @Input() highlightTerm = '';
-  @Input() partialSearch = false;
   @Output() readonly sortChanged = new EventEmitter<string>();
   @Output() readonly imageRequested = new EventEmitter<{
     id: string;
