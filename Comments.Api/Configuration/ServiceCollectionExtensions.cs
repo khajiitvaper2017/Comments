6b865d7a151895b88b8bb200861a3b5fb3d31246
khajiitvaper2017 <kanalgera@gmail.com>
2026-09-21 13:01:32 +0300

    remove unused packages

--- Comments.Api/Configuration/ServiceCollectionExtensions.cs
+++ Comments.Api/Configuration/ServiceCollectionExtensions.cs
@@ -22,7 +22,6 @@ public static class ServiceCollectionExtensions
         {
             services.AddHealthChecks();
             services.AddControllers().AddJsonOptions(options => { options.JsonSerializerOptions.MaxDepth = 64; });
-            services.AddOpenApi();
             return services;
         }
 
