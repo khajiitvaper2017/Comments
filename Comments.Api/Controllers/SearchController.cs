98d634f921b85695ce106e829c17e2cf9fa88a3b
khajiitvaper2017 <kanalgera@gmail.com>
2026-09-24 16:34:06 +0300

    simplify search controller

--- Comments.Api/Controllers/SearchController.cs
+++ Comments.Api/Controllers/SearchController.cs
@@ -11,17 +11,9 @@ public sealed class SearchController(ICommentSearch search) : ControllerBase
 {
     /// <summary>Searches comment text and user names through Elasticsearch.</summary>
     [HttpGet]
-    public Task<CommentPageDto> Get([FromQuery] string q,
-        [FromQuery] bool partial = false,
-        [FromQuery] bool searchText = true,
-        [FromQuery] bool searchUserName = true,
-        [FromQuery] bool searchComments = true,
-        [FromQuery] bool searchReplies = true,
-        [FromQuery] string? cursor = null,
+    public Task<CommentPageDto> Get([FromQuery] SearchCommentRequest request,
         CancellationToken ct = default)
     {
-        var request = new SearchCommentRequest(q, partial, searchText, searchUserName, searchComments,
-            searchReplies, cursor);
         return search.SearchAsync(request, ct);
     }
 }
